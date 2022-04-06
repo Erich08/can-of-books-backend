546384cd5d8fdf75e86b1c527ef6fec737a13198
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-04-05 23:16:14 -0500

    lab 12

--- server.js
+++ server.js
@@ -13,23 +13,40 @@ mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Mongoose is connected')
+db.once('open', function () {
+  console.log('Mongoose is connected');
 });
 
 const PORT = process.env.PORT || 3002;
 
 app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+  response.send('test request received');
+});
 
 app.get('/books', async (request, response) => {
-
   const books = await Book.find({});
-  
+
   response.send(books);
-})
+});
+
+app.delete('/books/:id', async (request, response) => {
+  try {
+    await Book.findByIdAndDelete(request.params.id);
+    response.status(204).send('Book was successfully deleted.');
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use(express.json());
+
+app.post('/books', async (request, response) => {
+  try {
+    const newBook = await Book.create(request.body);
+    response.status(204).send('Book was successfully created.');
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
